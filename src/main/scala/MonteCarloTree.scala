713f0363b4b29e90a1e626e0fb5d279cf6f2e5eb
Peter Brumbach <contact@peterbrumbach.com>
2026-04-27 23:34:50 -0400

    Monte Carlo search tree updated

--- src/main/scala/MonteCarloTree.scala
+++ src/main/scala/MonteCarloTree.scala
@@ -6,12 +6,12 @@ import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Random, Success}
 
 class MonteCarloTree {
-  def MonteCarloTree[A, B](decision: (Double, A) => A, isComplete: A => Boolean, winner: (A, B) => Boolean, currMove: A, player: B, isParallel: Boolean = true, iterations: Int = 20000000): A = {
+  def MonteCarloTree[A, B](decision: (Double, A, B) => A, isComplete: A => Boolean, winner: (A, B) => Boolean, currMove: A, player: B, isParallel: Boolean = true, iterations: Int = 20000000): A = {
 
     def SeqTree(): A = {
       val decisionMap = mutable.Map[A, Double]()
       for i <- 0 until iterations do {
-        val tryMove: A = decision(Random().nextDouble(), currMove)
+        val tryMove: A = decision(Random().nextDouble(), currMove, player)
         val wasSuccess: Boolean = RunScenario(tryMove)
         if wasSuccess then decisionMap(tryMove) = 1 + decisionMap.getOrElse(tryMove, 0.0)
       }
@@ -22,7 +22,7 @@ class MonteCarloTree {
       val decisionFutures: List[Future[(A, Boolean)]] = (
         for i <- 0 until iterations yield {
           Future {
-            val tryMove: A = decision(Random().nextDouble(), currMove)
+            val tryMove: A = decision(Random().nextDouble(), currMove, player)
             val wasSuccess: Boolean = RunScenario(tryMove)
             (tryMove, wasSuccess)
           }
@@ -36,7 +36,7 @@ class MonteCarloTree {
     }
 
     @tailrec
-    def RunScenario(currMove: A): Boolean = if isComplete(currMove) then winner(currMove, player) else RunScenario(decision(Random().nextDouble(), currMove))
+    def RunScenario(currMove: A): Boolean = if isComplete(currMove) then winner(currMove, player) else RunScenario(decision(Random().nextDouble(), currMove, player))
 
     if (isParallel) ParTree() else SeqTree()
   }
--- src/main/scala/TicTacToe.scala
+++ src/main/scala/TicTacToe.scala
@@ -1,9 +1,38 @@
-
+import scala.util.Random
 
 @main def findBestStartingMove(): Unit = {
 
 }
 
-def TicTacToeDecision(rand: Double, board: List[String]): List[String] = {
-  null
+def TicTacToeDecision(rand: Double, board: List[String], player: String): List[String] = {
+
+  def helper(r: Double, b: List[String], p: String): List[String] = {
+
+    var chosenSpot: Int = Math.round(r * b.count(_ == " ")).toInt
+    (for i <- b.indices yield {
+      if b(i) == " " then chosenSpot -= 1
+      if chosenSpot == -1 then {
+        chosenSpot = -2
+        p
+      } else b(i)
+    }).toList
+  }
+
+  val otherPlayer: String = if player == "x" then "o" else "x"
+  if board.count(_ == " ") > 1 then helper(Random().nextDouble(), helper(rand, board, player), otherPlayer)
+  else helper(rand, board, player)
+}
+
+def TicTacToeComplete(board: List[String]): Boolean = {
+  board.count(_ == " ") == 0
+}
+
+val board: List[String] =
+  ( " ", " ", " ",
+    " ", " ", " ",
+    " ", " ", " " ).toList
+
+
+def TicTacToeWinner(board: List[String], player: String): Boolean = {
+  true
 }
