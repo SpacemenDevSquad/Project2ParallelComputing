35a16986c534e1958986f8c1c2ae9b5e19823e53
Peter Brumbach <contact@peterbrumbach.com>
2026-04-28 16:55:41 -0400

    TicTacToe test run 1

--- src/main/scala/MonteCarloTree.scala
+++ src/main/scala/MonteCarloTree.scala
@@ -5,39 +5,38 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Random, Success}
 
-class MonteCarloTree {
-  def MonteCarloTree[A, B](decision: (Double, A, B) => A, isComplete: A => Boolean, winner: (A, B) => Boolean, currMove: A, player: B, isParallel: Boolean = true, iterations: Int = 20000000): A = {
+def MonteCarloTree[A, B](decision: (Double, A, B) => A, isComplete: A => Boolean, winner: (A, B) => Boolean, currMove: A, player: B, isParallel: Boolean = true, iterations: Int = 5000000): A = {
 
-    def SeqTree(): A = {
-      val decisionMap = mutable.Map[A, Double]()
-      for i <- 0 until iterations do {
-        val tryMove: A = decision(Random().nextDouble(), currMove, player)
-        val wasSuccess: Boolean = RunScenario(tryMove)
-        if wasSuccess then decisionMap(tryMove) = 1 + decisionMap.getOrElse(tryMove, 0.0)
-      }
-      decisionMap.maxBy(_._2)._1
+  def SeqTree(): A = {
+    val decisionMap = mutable.Map[A, Double]()
+    for i <- 0 until iterations do {
+      val tryMove: A = decision(Random().nextDouble(), currMove, player)
+      val wasSuccess: Boolean = RunScenario(tryMove)
+      if wasSuccess then decisionMap(tryMove) = 1 + decisionMap.getOrElse(tryMove, 0.0)
     }
-
-    def ParTree(): A = {
-      val decisionFutures: List[Future[(A, Boolean)]] = (
-        for i <- 0 until iterations yield {
-          Future {
-            val tryMove: A = decision(Random().nextDouble(), currMove, player)
-            val wasSuccess: Boolean = RunScenario(tryMove)
-            (tryMove, wasSuccess)
-          }
-      }).toList
-
-      val seqFuture: Future[List[(A, Boolean)]] = Future.sequence(decisionFutures)
-      Await.ready(seqFuture, Duration.Inf).value.get match {
-        case Failure(exception) => throw exception
-        case Success(answer) => answer.filter(_._2).map[A](_._1).groupBy(value => value).maxBy(_._2.size)._1
-      }
-    }
-
-    @tailrec
-    def RunScenario(currMove: A): Boolean = if isComplete(currMove) then winner(currMove, player) else RunScenario(decision(Random().nextDouble(), currMove, player))
-
-    if (isParallel) ParTree() else SeqTree()
+    decisionMap.maxBy(_._2)._1
   }
+
+  def ParTree(): A = {
+    val decisionFutures: List[Future[(A, Boolean)]] = (
+      for i <- 0 until iterations yield {
+        Future {
+          val tryMove: A = decision(Random().nextDouble(), currMove, player)
+          val wasSuccess: Boolean = RunScenario(tryMove)
+          println("Iteration "+i+" complete")
+          (tryMove, wasSuccess)
+        }
+    }).toList
+
+    val seqFuture: Future[List[(A, Boolean)]] = Future.sequence(decisionFutures)
+    Await.ready(seqFuture, Duration.Inf).value.get match {
+      case Failure(exception) => throw exception
+      case Success(answer) => answer.filter(_._2).map[A](_._1).groupBy(value => value).maxBy(_._2.size)._1
+    }
+  }
+
+  @tailrec
+  def RunScenario(currMove: A): Boolean = if isComplete(currMove) then winner(currMove, player) else RunScenario(decision(Random().nextDouble(), currMove, player))
+
+  if (isParallel) ParTree() else SeqTree()
 }
--- src/main/scala/TicTacToe.scala
+++ src/main/scala/TicTacToe.scala
@@ -1,14 +1,19 @@
 import scala.util.Random
 
-@main def findBestStartingMove(): Unit = {
+val startBoard: List[String] =
+  ( " ", " ", " ",
+    " ", " ", " ",
+    " ", " ", " " ).toList
 
+@main def findBestStartingMove(): Unit = {
+  println(MonteCarloTree[List[String], String](TicTacToeDecision, TicTacToeComplete, TicTacToeWinner, startBoard, "x"))
 }
 
 def TicTacToeDecision(rand: Double, board: List[String], player: String): List[String] = {
 
   def helper(r: Double, b: List[String], p: String): List[String] = {
 
-    var chosenSpot: Int = Math.round(r * b.count(_ == " ")).toInt
+    var chosenSpot: Int = Math.round(r * (b.count(_ == " ") - 1)).toInt
     (for i <- b.indices yield {
       if b(i) == " " then chosenSpot -= 1
       if chosenSpot == -1 then {
@@ -24,15 +29,17 @@ def TicTacToeDecision(rand: Double, board: List[String], player: String): List[S
 }
 
 def TicTacToeComplete(board: List[String]): Boolean = {
-  board.count(_ == " ") == 0
+  board.count(_ == " ") == 0 || TicTacToeWinner(board, "x") || TicTacToeWinner(board, "o")
 }
-
-val board: List[String] =
-  ( " ", " ", " ",
-    " ", " ", " ",
-    " ", " ", " " ).toList
 
 
 def TicTacToeWinner(board: List[String], player: String): Boolean = {
-  true
+  var isTrue = false
+  for i <- 0 to 2 do {
+    if board(0+i*3) == player && board(1+i*3) == player && board(2+i*3) == player then isTrue = true
+    if board(i) == player && board(i+3) == player && board(i+6) == player then isTrue = true
+  }
+  if board.head == player && board(4) == player && board(8) == player then isTrue = true
+  if board(2) == player && board(4) == player && board(6) == player then isTrue = true
+  isTrue
 }
